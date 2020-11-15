8bfb5cebf31f1d27ccfa0d214f82af3d1815c5f0
bionade24 <o.roesler@oscloud.info>
2020-11-15 18:43:42 +0100

    Make custom admin themes possible and add custom base_site.html for admin app

--- abs_cd/settings.py
+++ abs_cd/settings.py
@@ -65,7 +65,7 @@ ROOT_URLCONF = 'abs_cd.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, "templates"), ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
