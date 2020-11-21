69b61d39fec10db090170f652f13dbd7199c95f4
Felix Exner <exner@fzi.de>
2020-11-21 22:02:04 +0100

    Use built_packages also for non-rebuild
    As the dependency graph is not necessarily acyclic we have to make sure to check each node
    only once. Otherwise this might end up in an endless loop (meaning we will hit the
    recursion limit)

--- cd_manager/tests.py
+++ cd_manager/tests.py
@@ -1,3 +1,31 @@
 from django.test import TestCase
+from cd_manager.models import Package
+from cd_manager.alpm import ALPMHelper, PackageNotFoundError
+from cd_manager.recursion_helper import Recursionlimit
 
-# Create your tests here.
+# Prototype implementation for Package.build() to check the visitor does the job
+def get_deps(pkgname, level=0, visited=[]):
+    if pkgname in visited:
+        return
+    visited.append(pkgname)
+    try:
+        deps = ALPMHelper().get_deps(pkgname=pkgname, rundeps=True, makedeps=True)
+    except PackageNotFoundError as err:
+        print(" "*level + str(err))
+        return
+    print(" "*level + pkgname + " has " + str(len(deps)) + " dependencies.")
+
+    with Recursionlimit(2000):
+        for dep in deps:
+            dep = Package.sanitize_dep(dep)
+            get_deps(dep, level+1, visited)
+
+class TestRecursiveDeps(TestCase):
+
+    # A ROS package with very minimal depencencies
+    def test_ros_melodic_genmsg(self):
+        get_deps("ros-melodic-genmsg")
+
+    # A ROS package with a lot of dependencies
+    def test_ros_melodic_desktop_full(self):
+        get_deps("ros-melodic-desktop-full")
