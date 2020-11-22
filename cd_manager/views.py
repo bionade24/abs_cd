770369507b83974a0525b0d406e6f5d78f780288
bionade24 <o.roesler@oscloud.info>
2020-11-22 12:11:57 +0100

    Make views.PackageOverview sortable

--- cd_manager/views.py
+++ cd_manager/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponse
 from django.views import generic
+from sortable_listview import SortableListView
 
 from .models import Package
 
@@ -11,6 +12,14 @@ def package(request, package_name):
      context={'package': p})
 
 
-class PackageOverview(generic.ListView):
+class PackageOverview(SortableListView):
     model = Package
     context_object_name = 'package_list'
+    allowed_sort_fields = {'name': {'default_direction': '',
+                                    'verbose_name': 'Package name'},
+                           'build_status': {'default_direction': '',
+                                    'verbose_name': 'Build status'},
+                           'build_date': {'default_direction': '-',
+                                    'verbose_name': 'Build date'}
+                           }
+    default_sort_field = 'name'
