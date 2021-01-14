ec6d229037bf0bfb8f3c5a1fa4997a71ac44ce01
bionade24 <o.roesler@oscloud.info>
2021-01-14 21:30:28 +0100

    Add logging message when cronjob is triggered

--- cd_manager/cron.py
+++ cd_manager/cron.py
@@ -1,6 +1,7 @@
 from cd_manager.models import Package
 
 def check_for_new_commits():
+    print("Start checking for new commits in all repos.")
     for pkg in Package.objects.all():
         if pkg.repo_status_check():
             pkg.build()
