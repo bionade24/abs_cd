68746d4dbde06a799228ac84178f973dbbf3a5bb
bionade24 <o.roesler@oscloud.info>
2021-03-27 16:33:42 +0100

    If django-cronjob raises an exception, log it with the python logging driver

--- cd_manager/cron.py
+++ cd_manager/cron.py
@@ -7,8 +7,11 @@ logger = logging.getLogger(__name__)
 
 def check_for_new_commits():
     logger.info("Start checking for new commits in all repos.")
-    for pkg in Package.objects.all():
-        if pkg.repo_status_check():
-            pkg.build()
-            #TODO: Speed up process by already excluding dependencies after pkg was built
+    try:
+        for pkg in Package.objects.all():
+            if pkg.repo_status_check():
+                pkg.build()
+                #TODO: Speed up process by already excluding dependencies after pkg was built
+    except BaseException:
+        logger.exception("Cronjob checking for new commits in all repos failed:")
 
