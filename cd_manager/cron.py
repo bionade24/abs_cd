aeabd3d7552a7400f6a320017c2f9f25e2de6343
Oskar Roesler (bionade24) <o.roesler@oscloud.info>
2021-07-03 17:58:13 +0200

    Fix str.join() in cron.py by putting all args in a list.

--- cd_manager/cron.py
+++ cd_manager/cron.py
@@ -15,7 +15,7 @@ def check_for_new_pkgversions():
             db_pkginfo = ALPMHelper().get_pkg_from_syncdbs(pkg.name)
             srcinfo = ALPMHelper.get_srcinfo(pkg.name).getcontent()
             # vercmp < 0 mean input 2 is higher, see man vercmp
-            if vercmp(db_pkginfo.version, str.join(srcinfo['pkgver'], '-', srcinfo['pkgrel'])) < 0:
+            if vercmp(db_pkginfo.version, str.join([srcinfo['pkgver'], '-', srcinfo['pkgrel']])) < 0:
                 pkg.build(repo_status_check=False)
     except BaseException:
         logger.exception("Cronjob checking for new pkg versions in all repos failed:")
