60337b95e5cc86aca4cecb167a883d852b0ca49e
Oskar Roesler (bionade24) <o.roesler@oscloud.info>
2021-08-24 21:38:31 +0200

    In makepkg/docker_conn.py: Assign Docker socket connection to static __docker_conn var to act similar to a singleton.

--- makepkg/docker_conn.py
+++ makepkg/docker_conn.py
@@ -11,10 +11,10 @@ class Connection():
 
     def __new__(self):
         if not self.__docker_conn:
-            self.__docker_conn = docker.DockerClient(base_url=Confighelper()
-                                                     .get_setting('DOCKER_SOCKET',
-                                                                  'unix:///var/run/docker.sock'),
-                                                     version='auto', tls=False)
+            Connection.__docker_conn = docker.DockerClient(base_url=Confighelper()
+                                                           .get_setting('DOCKER_SOCKET',
+                                                                        'unix:///var/run/docker.sock'),
+                                                           version='auto', tls=False)
             logger.debug("Successfully established connection")
         else:
             logger.debug("Connection to Docker socket already established")
