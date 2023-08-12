56b43d57d394af5a4e564b0024869006d4b313c8
Oskar Roesler (bionade24) <o.roesler@oscloud.info>
2023-08-12 12:14:08 +0200

    Django settings.py: Set DEFAULT_AUTO_FIELD to BigAutoField.

--- abs_cd/settings.py
+++ abs_cd/settings.py
@@ -95,6 +95,8 @@ DATABASES = {
     }
 }
 
+DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
