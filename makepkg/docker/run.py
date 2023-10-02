29e2d91e9040223c1ca5f8a316a2a4422860b7d6
Oskar Roesler (bionade24) <o.roesler@oscloud.info>
2023-10-02 23:12:03 +0200

    makepkg: Use JSON encoded build result.
    The JSON msg is sent from the build container on finishing or erroring
    out. It contains the exitcode, a list of the built packages and the
    build log. Remove the directory before build / after build logic as it
    is not needed anymore.

--- makepkg/docker/run.py
+++ makepkg/docker/run.py
@@ -1,17 +1,20 @@
-#! /bin/python3 -B
+#!/usr/bin/env -S python -B
 
 import argparse
 import glob
+import json
 import os
-import os.path
 import pwd
 import subprocess
 import shutil
 import sys
+import traceback
 from subprocess import PIPE, STDOUT
 
 USERNAME = 'builder'
 USERDIR = '/builder'
+
+build_log = ""
 
 
 def chown_recursive(uid, gid, path):
@@ -24,15 +27,19 @@ def chown_recursive(uid, gid, path):
     os.chown(path, uid, gid)
     for root, dirs, files in os.walk(path):
         for momo in dirs:
-            try:
-                os.chown(os.path.join(root, momo), uid, gid)
-            except Exception as e:
-                print(e, file=sys.stderr)
+            os.chown(os.path.join(root, momo), uid, gid)
         for file in files:
-            try:
-                os.chown(os.path.join(root, file), uid, gid)
-            except Exception as e:
-                print(e, file=sys.stderr)
+            os.chown(os.path.join(root, file), uid, gid)
+
+
+def log(msg: str):
+    global build_log
+    build_log += (msg + "\n")
+
+
+def print_and_exit(extcode: int, built_pkgs: list[str] | None = None) -> None:
+    print(json.dumps({'exitcode': extcode, 'build_log': build_log, 'built_pkgs': built_pkgs}))
+    sys.exit(extcode)
 
 
 def main():
@@ -54,8 +61,8 @@ def main():
     parser_args, makepkg_extra_flags = parser.parse_known_args()
 
     if not os.path.isfile("/src/PKGBUILD") or os.path.islink("/src/PKGBUILD"):
-        print("No PKGBUILD file found! Aborting.", file=sys.stderr)
-        sys.exit(1)
+        log("No PKGBUILD file found! Aborting.")
+        print_and_exit(2)
 
     src_folder = "/src"
     for entry in os.listdir(src_folder):
@@ -72,31 +79,41 @@ def main():
     pacman_args = ['pacman', '--noconfirm', '-Sy']
     if parser_args.sysupgrade:
         pacman_args.append('-u')
-    pacman_proc = subprocess.run(pacman_args, stdout=PIPE, stderr=STDOUT)
+    pacman_proc = subprocess.run(pacman_args, stdout=PIPE, stderr=STDOUT, encoding='UTF-8')
     if pacman_proc.returncode != 0:
-        print(pacman_proc.stdout.decode('UTF-8'), file=sys.stderr)
+        log("pacman exited with error:")
+        log(pacman_proc.stdout)
 
     # if a command is specified with --execute, then run it
     if parser_args.execute:
-        if subprocess.run(['su', '-c', parser_args.execute, USERNAME]).returncode != 0:
-            print(f"Custom command \"{parser_args.execute}\" failed. Aborting.", file=sys.stderr)
-            sys.exit(1)
+        cmd = subprocess.run(['su', '-c', parser_args.execute, USERNAME], stdout=PIPE, stderr=STDOUT, encoding='UTF-8')
+        if cmd.returncode != 0:
+            log(cmd.stdout)
+            log(f"Custom command \"{parser_args.execute}\" failed. Aborting.")
+            print_and_exit(2)
 
     # If subprocess.Popen(user=uid) is used, gpg auto-key-retrieve doesn't work
     makepkg_args = ['su', '-c', "makepkg --force --syncdeps --noconfirm " +
                     " ".join(makepkg_extra_flags), USERNAME]
-    if subprocess.run(makepkg_args).returncode != 0:
-        sys.exit(2)
+    makepkg_proc = subprocess.run(makepkg_args, stdout=PIPE, stderr=STDOUT, encoding='UTF-8')
+    log(makepkg_proc.stdout)
+    if makepkg_proc.returncode != 0:
+        sys.exit(1)
 
-    built_packages = glob.iglob(os.path.join(USERDIR, "*.pkg.tar.*"))
+    built_packages = glob.glob(os.path.join(USERDIR, "*.pkg.tar.*"))
     if not built_packages:
-        print("No packages were built!", file=sys.stderr)
-        sys.exit(2)
+        log("No packages were built!")
+        print_and_exit(1)
     else:
         for file in built_packages:
             shutil.copy(file, "/repo")
-    sys.exit(0)
+        # Only get the filenames, not their full path
+        print_and_exit(0, list(map(os.path.basename, built_packages)))
 
 
 if __name__ == "__main__":
-    main()
+    try:
+        main()
+    except Exception:
+        log(traceback.format_exc())
+        print_and_exit(2)
