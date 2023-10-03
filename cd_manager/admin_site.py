0b37716f02bf5602468524d4c8b30737aca8691f
Oskar Roesler (bionade24) <o.roesler@oscloud.info>
2023-10-03 16:43:39 +0200

    admin: Paginate the log of the cd_manager webinterface.

--- cd_manager/admin_site.py
+++ cd_manager/admin_site.py
@@ -1,6 +1,6 @@
 import logging
 from django.contrib import admin
-from django.conf.urls import url
+from django.urls import re_path
 from django.shortcuts import render
 from makepkg.docker_conn import Connection
 
@@ -12,8 +12,8 @@ class CustomAdminSite(admin.AdminSite):
     def get_urls(self):
         urls = super().get_urls()
         custom_urls = [
-                url(
-                    'abs_cd_log',
+                re_path(
+                    r'^abs_cd_log$',
                     self.admin_view(self.show_abs_cd_log),
                     name='abs_cd_log',
                 ),
@@ -22,14 +22,37 @@ class CustomAdminSite(admin.AdminSite):
 
     def show_abs_cd_log(self, request, *args, **kwargs):
         request.current_app = self.name
+        page = request.GET.get('page')
+        LINES_PP = 500
         try:
             container = Connection().containers.list(filters={"label": "org.abs-cd=webcd_manager"})[0]
-            log = container.logs().decode('utf-8')
+            full_log = container.logs().decode('utf-8').splitlines(keepends=True)
+            page_count = 1 + len(full_log)//LINES_PP
+            logger.debug(f"Requested page nr: {page} page count: {page_count}")
+            try:
+                page = int(page)
+            except (ValueError, TypeError):
+                logger.debug("Can't convert page nr {page} to int, assuming latest instead.")
+                page = page_count
+            if page >= page_count:
+                text = full_log[-LINES_PP:]
+                page = page_count
+            elif isinstance(page, int) and page in range(1, page_count):
+                lines_after = -LINES_PP*(page_count - page)
+                lines_shown = lines_after - LINES_PP
+                text = full_log[lines_shown:lines_after]
+            else:
+                raise RuntimeError(f"Page {page} can't be found.")
+            text = "".join(text)
         except BaseException:
-            log = None
-            logger.exception("Getting log from container failed.")
+            logger.exception("Getting log from container failed:")
+            text = None
+            page_count = 1
+            page = 1
         return render(request, 'admin/abs_cd_log.html',
-                      context={'log': log})
+                      context={'log': text, 'page': page, 'page_count': page_count,
+                               'prev_page': page - 1 if page > 1 else None,
+                               'next_page': page + 1 if page < page_count else None, })
 
 
 site = CustomAdminSite()
