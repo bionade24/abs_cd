1636990d685db58bc747f947e338cd397a4487d9
Oskar Roesler (bionade24) <o.roesler@oscloud.info>
2023-12-29 18:32:24 +0100

    cd_manager: Set build statuses of the execution phase to NOT_BUILT during application startup.

--- cd_manager/apps.py
+++ cd_manager/apps.py
@@ -1,9 +1,21 @@
+import sys
 from django.apps import AppConfig
 from django.contrib.admin.apps import AdminConfig
 
 
 class CdManagerConfig(AppConfig):
     name = 'cd_manager'
+
+    def ready(self):
+        if "runserver" in sys.argv:
+            from cd_manager.models import Package
+            pkgs = Package.objects.filter(build_status="BUILDING").union(
+            Package.objects.filter(build_status="PREPARIG"),
+            Package.objects.filter(build_status="WAITING"))
+            for pkg in pkgs:
+                pkg.build_status = "NOT_BUILT"
+                pkg.save()
+
 
 
 class Abs_cd_AdminSite(AdminConfig):
