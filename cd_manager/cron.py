c3ee58470b6f00075cb82257ae0c768554bda13d
Oskar Roesler (bionade24) <o.roesler@oscloud.info>
2024-08-22 16:56:16 +0200

    Handle pkg.repo_status_check() exceptions in the cronjob.
    Don't fail the whole check_for_new_pkgversions() cronjob if the git
    operations of one pkg fails, continue to proceed with the next pkg.

--- cd_manager/cron.py
+++ cd_manager/cron.py
@@ -9,23 +9,25 @@ logger = logging.getLogger(__name__)
 
 def check_for_new_pkgversions():
     logger.info("Start checking for new pkg versions in all repos.")
-    try:
-        for pkg in Package.objects.all():
+    for pkg in Package.objects.all():
+        try:
             pkg.pkgbuild_repo_status_check()
+        except BaseException:
+            logger.exception(f"Checking if repo of {pkg.name} is up-to-date failed:")
+            continue
+        try:
+            db_pkginfo_version = ALPMHelper().get_pkg_from_syncdbs(pkg.name).version
+        except PackageNotFoundError:
+            db_pkginfo_version = '-1'
+        srcinfo = ALPMHelper.get_srcinfo(pkg.name).getcontent()
+        srcinfo_version = f"{srcinfo['pkgver']}-{srcinfo['pkgrel']}"
+        # vercmp < 0 mean input 2 is higher, see man vercmp
+        if vercmp(db_pkginfo_version, srcinfo_version) < 0:
             try:
-                db_pkginfo_version = ALPMHelper().get_pkg_from_syncdbs(pkg.name).version
-            except PackageNotFoundError:
-                db_pkginfo_version = '-1'
-            srcinfo = ALPMHelper.get_srcinfo(pkg.name).getcontent()
-            srcinfo_version = f"{srcinfo['pkgver']}-{srcinfo['pkgrel']}"
-            # vercmp < 0 mean input 2 is higher, see man vercmp
-            if vercmp(db_pkginfo_version, srcinfo_version) < 0:
-                try:
-                    pkg.build(repo_status_check=False)
-                except BaseException:
-                    logger.exception(f"An automatic build of package {pkg.name} was triggered but failed")
-    except BaseException:
-        logger.exception("Cronjob checking for new pkg versions in all repos failed:")
+                pkg.build(repo_status_check=False)
+            except BaseException:
+                logger.exception(f"An automatic build of package {pkg.name} was triggered but failed")
+                continue  # Explicitness in code is good
 
 
 def update_pacmandbs():
