49788a8b7766a0d0016f96bb2affbc83cfa13da0
Oskar Roesler (bionade24) <o.roesler@oscloud.info>
2024-08-28 12:44:09 +0200

    Fix the reset of execution build statuses on startup.

--- cd_manager/apps.py
+++ cd_manager/apps.py
@@ -7,17 +7,15 @@ class CdManagerConfig(AppConfig):
     name = 'cd_manager'
 
     def ready(self):
-        if "runserver" in sys.argv:
+        if ("manage.py" not in sys.argv) or ("runserver" in sys.argv):
             from cd_manager.models import Package
             pkgs = Package.objects.filter(build_status="BUILDING").union(
-            Package.objects.filter(build_status="PREPARIG"),
+            Package.objects.filter(build_status="PREPARING"),
             Package.objects.filter(build_status="WAITING"))
             for pkg in pkgs:
                 pkg.build_status = "NOT_BUILT"
                 pkg.save()
 
 
-
 class Abs_cd_AdminSite(AdminConfig):
     default_site = 'cd_manager.admin_site.get_site'
-
